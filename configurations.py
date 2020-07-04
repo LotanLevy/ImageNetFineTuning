0cdc0ba0b07126591d88aa235582d22b1de6bc73
LotanLevy <lotan.levy@mail.huji.ac.il>
2020-07-04 15:12:09 +0300

    imagenet training files

--- configurations.py
+++ configurations.py
@@ -11,11 +11,11 @@ def get_args():
     parser.add_argument('--train_path', type=str, required=True)
     parser.add_argument('--val_path', type=str, required=True)
     parser.add_argument('--test_path', type=str, required=True)
-    parser.add_argument('--output_path', '-o', type=str, default=os.getcwd(), help='The path to keep the output')
+    parser.add_argument('--output_path', type=str, default=os.getcwd(), help='The path to keep the output')
     parser.add_argument('--print_freq', '-pf', type=int, default=10)
     parser.add_argument('--lr', type=float, default=5e-5, help='learning rate')
     parser.add_argument('--batchs_num', '-bs', type=int, default=2, help='number of batches')
-
+    return parser.parse_args()
 
 
 
